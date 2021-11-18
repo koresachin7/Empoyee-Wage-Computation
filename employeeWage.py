45f5ee6a2e9c1de5d518a6628f1f1f430694fec8
koresachin7 <koresachin7@gmail.com>
2021-11-18 13:38:42 +0530

    Added : - using Switcher to choice Employee Options

--- employeeWage.py
+++ employeeWage.py
@@ -1,39 +1,52 @@
 """
 * @Author: Sachin S Kore
 * @Date: 2021-11-16
-* @Title : To check employee Full time , part time or absent
+* @Title :  To check employee is present or absent using switcher also
 """
 import random
-print("Welcome to employee wage computation")
 
 
-def check_attendance():
-    """
-       Description:
-           This function is to check whether employee is present or absent.
-           Employee daily wage is calculated for full day and part time.
-       """
+class Employee:
 
-    isFullTime = 1
-    isPartTime = 2
-    day_Hour = 0
+    def check_attendance(e):
+        """
+          Description:
+              This function is to check whether employee is present or absent.
+              Employee daily wage is calculated for full day and part time.
+          """
 
-    wage_Per_Hour = 20
+        isFullTime = 1
+        isPartTime = 2
+        day_Hour = 0
 
-    emp_Check = random.randint(0, 2)
+        wage_Per_Hour = 20
+        if emp_check == isFullTime:
+            day_Hour = 8
+        elif emp_check == isPartTime:
+            day_Hour = 4
+        else:
+            print("Employee is absent")
 
-    if emp_Check == isFullTime:
-        print("Employee is present Full time..... ")
-        day_Hour = 8
-    elif emp_Check == isPartTime:
-        print("Employee is present Part time..... ")
-        day_Hour = 4
-    else:
-        print("Employee is absent......")
+        employee_Wage = wage_Per_Hour * day_Hour
+        print("Employee wage is:", employee_Wage)
 
-    employee_Wage = wage_Per_Hour * day_Hour
-    print("Employee wage is:", employee_Wage)
+    def switch(x):
+        absent = 0
+        full_present = 1
+        part_present = 2
+
+        switcher = {
+            0: absent,
+            1: full_present,
+            2: part_present,
+        }
+        attendance = switcher.get(emp_check)
+        print(attendance)
 
 
 if __name__ == "__main__":
-    check_attendance()
+    print("Welcome to employee wage computation")
+    emp_check = random.randint(0, 2)
+    a = Employee()
+    a.check_attendance()
+    a.switch()
