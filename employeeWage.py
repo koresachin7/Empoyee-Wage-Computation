6eafab4657ca960926bac6b995d4ef27ffd65300
koresachin7 <koresachin7@gmail.com>
2021-11-19 14:09:47 +0530

    Modified : - Method and variable modified in the program

--- employeeWage.py
+++ employeeWage.py
@@ -8,21 +8,21 @@ import random
 
 class Employee:
 
-    def check_attendance(e):
+    def check_attendance(att):
         """
           Description:
               This function is to check whether employee is present or absent.
               Employee daily wage is calculated for full day and part time.
           """
 
-        isFullTime = 1
-        isPartTime = 2
+        is_full_time = 1
+        is_part_time = 2
         day_Hour = 0
 
         wage_Per_Hour = 20
-        if emp_check == isFullTime:
+        if emp_check == is_full_time:
             day_Hour = 8
-        elif emp_check == isPartTime:
+        elif emp_check == is_part_time:
             day_Hour = 4
         else:
             print("Employee is absent")
@@ -30,7 +30,7 @@ class Employee:
         employee_Wage = wage_Per_Hour * day_Hour
         print("Employee wage is:", employee_Wage)
 
-    def switch(x):
+    def switch(emp_check):
         absent = 0
         full_present = 1
         part_present = 2
@@ -41,12 +41,12 @@ class Employee:
             2: part_present,
         }
         attendance = switcher.get(emp_check)
-        print(attendance)
+        return attendance
 
 
 if __name__ == "__main__":
     print("Welcome to employee wage computation")
     emp_check = random.randint(0, 2)
-    a = Employee()
-    a.check_attendance()
-    a.switch()
+    att = Employee.switch(emp_check)
+    Employee.check_attendance(att)
+
